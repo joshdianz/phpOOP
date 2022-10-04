ccf817642392db55dcae2c00c9c1676136b69486
joshdianz <87035713+joshdianz@users.noreply.github.com>
2022-10-04 21:14:39 +0200

    Update index.php

--- PHP OOP/classesAndObjects/index.php
+++ PHP OOP/classesAndObjects/index.php
@@ -15,13 +15,12 @@ class Car{
     public $comp;
     public $color = 'beige';
     public $hasSunRoof = true;
-
+}
     $bmw = new Car();
     $mercedes = new Car();
 
     echo $bmw -> color;
     echo $mercedes -> color;
-}
 ?>
 </body>
 </html>
